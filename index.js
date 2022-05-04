f43c1677bec813c36ced3880e883de5388e8c2fb
alanli22 <103334127+alanli22@users.noreply.github.com>
2022-05-04 22:41:15 +0100

    styles, JS and index files

---
+++ index.js
@@ -0,0 +1,37 @@
+
+//destinations slider
+$('.destinations').slick({
+	slidesToShow: 6,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	infinite: true,
+	speed: 300,
+	variableWidth: true,
+	arrows: false,
+	dots: false
+
+});
+
+//destinations slider
+$('.categories').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	infinite: true,
+	speed: 300,
+	variableWidth: true,
+	arrows: false,
+	dots: false
+
+});
+//image slider
+$('.image-slider').slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	centerMode: true,
+	arrows: true,
+	variableWidth: true
+
+});
